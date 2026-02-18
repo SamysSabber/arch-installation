f4cece4ce8478d0fcf555a257123f47f0aaf09c4
SamysSabber <118390382+SamysSabber@users.noreply.github.com>
2026-02-18 03:00:16 +0100

    Add files via upload

--- install.sh
+++ install.sh
@@ -1,7 +1,42 @@
+#!/bin/bash
+
 #Festplatte wählen
-lsblk
-read -p "Festplatte wählen: (nicht nvme0nXp oder sdX) " DISK
-read -p "Hostname für das Gerät setzen " HOSTNAME
+echo "Verfügbare Festplatten:"
+lsblk -d -o NAME,SIZE,TYPE | grep disk
+echo ""
+read -p "Festplattenname eingeben (z.b. sda, vda, nvme0n1): " DISKNAME
+DISK="/dev/$DISKNAME"
+
+#Prüfen ob Disk existiert
+if [ ! -b "$DISK" ]; then
+  echo "Fehler: $DISK existiert nicht!"
+  exit 1
+fi
+
+#NVMe Partitionsbezeichnung anpassen (nvme0n1 → nvme0n1p1, sda → sda1)
+if [[ $DISKNAME == nvme* ]]; then
+  PART1="${DISK}p1"
+  PART2="${DISK}p2"
+else
+  PART1="${DISK}1"
+  PART2="${DISK}2"
+fi
+
+echo "Disk: $DISK"
+echo "Boot Partition: $PART1"
+echo "Root Partition: $PART2"
+read -p "Korrekt? (j/n) " CONFIRM
+if [ "$CONFIRM" != "j" ]; then
+  echo "Abgebrochen."
+  exit 1
+fi
+
+read -p "Hostname für das Gerät setzen: " HOSTNAME
+read -p "Benutzername: " USERNAME
+read -sp "Root Passwort: " ROOTPASS
+echo
+read -sp "Benutzer Passwort: " USERPASS
+echo
 
 #Deutsche Tastatur laden im Archiso environment
 loadkeys de-latin1
@@ -18,22 +53,22 @@ sgdisk -n 1:0:+1G -t 1:ef00 $DISK
 sgdisk -n 2:0:0 -t 2:8300 $DISK
 
 #Partitionen formatieren
-mkfs.fat -F32 ${DISK}1
-mkfs.btrfs -f ${DISK}2
+mkfs.fat -F32 $PART1
+mkfs.btrfs -f $PART2
 
 #Subvolumes auf Root erstellen
-mount /dev/${DISK}2 /mnt
+mount $PART2 /mnt
 btrfs subvolume create /mnt/@
 btrfs subvolume create /mnt/@home
 btrfs subvolume create /mnt/@snapshots
 umount /mnt
 
 #Subvolumes und EFI mounten
-mount -t btrfs -o subvol=@,compress=zstd /dev/${DISK}2 /mnt
+mount -t btrfs -o subvol=@,compress=zstd $PART2 /mnt
 mkdir -p /mnt/{boot,home,.snapshots}
-mount /dev/vda1 /mnt/boot
-mount -t btrfs -o subvol=@home,compress=zstd /dev/${DISK}2 /mnt/home
-mount -t btrfs -o subvol=@snapshots,compress=zstd /dev/${DISK}2 /mnt/.snapshots
+mount $PART1 /mnt/boot
+mount -t btrfs -o subvol=@home,compress=zstd $PART2 /mnt/home
+mount -t btrfs -o subvol=@snapshots,compress=zstd $PART2 /mnt/.snapshots
 
 #Basissystem installieren
 pacstrap -K /mnt base linux linux-firmware vim sudo
@@ -45,45 +80,44 @@ cp /etc/pacman.d/mirrorlist /mnt/etc/pacman.d/mirrorlist
 genfstab -U /mnt >>/mnt/etc/fstab
 
 #Chroot ins Live-system
-arch-chroot /mnt
+arch-chroot /mnt /bin/bash <<EOF
 
 #Zeitzone im Livesystem anpassen
 ln -sf /usr/share/zoneinfo/Europe/Berlin /etc/localtime
 hwclock --systohc
 
 #Sprache im Livesystem setzen
-echo "de_DE.UTF-8" >/etc/locale.gen
+echo "de_DE.UTF-8 UTF-8" > /etc/locale.gen
 locale-gen
-echo "LANG=de_DE.UTF-8" >/etc/locale.conf
-echo "KEYMAP=de-latin1" >/etc/vconsole.conf
+echo "LANG=de_DE.UTF-8" > /etc/locale.conf
+echo "KEYMAP=de-latin1" > /etc/vconsole.conf
 
 #Hostname setzen
-echo $HOSTNAME >/etc/hostname
+echo "$HOSTNAME" > /etc/hostname
 
-#Root Passwort setzten
-echo "Root passwort setzen: "
-passwd
+#Root Passwort setzen
+echo "root:$ROOTPASS" | chpasswd
 
 #Benutzer erstellen
-useradd -mG wheel hamster
-echo "Benutzter Passwort setzen: "
-passwd hamster
-sed -i 's/# %wheel ALL=(ALL:ALL) ALL/%wheel ALL=(ALL:ALL) ALL/' /etc/sudoers #Erlaubt sudo befehle für Gruppe wheel
+useradd -mG wheel $USERNAME
+echo "$USERNAME:$USERPASS" | chpasswd
+sed -i 's/# %wheel ALL=(ALL:ALL) ALL/%wheel ALL=(ALL:ALL) ALL/' /etc/sudoers #Erlaubt sudo Befehle für Gruppe wheel
 
-#Netzwork
-pacman -S networkmanager
+#Netzwerk
+pacman -S networkmanager --noconfirm
 systemctl enable NetworkManager
 
 #Grub
-pacman -S grub efibootmgr
+pacman -S grub efibootmgr --noconfirm
 grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
 grub-mkconfig -o /boot/grub/grub.cfg
 
 #Hyprland Standart
-pacman -S hyprland xdg-desktop-portal-hyprland waybar wofi kitty pipewire wireplumber polkit-kde-agent gdm
+pacman -S hyprland xdg-desktop-portal-hyprland waybar wofi kitty pipewire wireplumber polkit-kde-agent gdm --noconfirm
 systemctl enable gdm
 
+EOF
+
 #Unmounten und Reboot
-exit
 umount -R /mnt
 reboot
