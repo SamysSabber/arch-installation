b4b0d293964a0a635068926c66ea4c867c3ca4bb
SamysSabber <118390382+SamysSabber@users.noreply.github.com>
2026-02-18 14:39:40 +0100

    Add files via upload

---
+++ post-install.sh
@@ -0,0 +1,18 @@
+#Apps
+sudo pacman -S alacritty neovim obsidian bitwarden steam btop \
+    spotify-launcher hyprshot hyprpolkitagent --noconfirm
+
+yay -S visual-studio-code-bin vesktop zen-browser-bin peaclock protonup-qt brave-bin --noconfirm
+
+#Fonts und Icons
+sudo pacman -S ttf-jetbrains-mono-nerd noto-fonts-emoji --noconfirm
+
+#Sound Utils
+sudo pacman -S pipewire pavucontrol easyeffects wireplumber pipewire-pulse --noconfirm
+sudo systemctl --user enable --now pipewire pipewire-pulse wireplumber
+
+#VM QEMU/KVM
+sudo pacman -S qemu-full libvirt virt-manager --noconfirm
+sudo systemctl enable --now libvirtd
+sudo usermod -aG libvirt $USER
+
